ed2f576d5122e7a8d54c4f288658397d57d66368
John Allison <jeallis2@ncsu.edu>
2020-11-07 20:01:56 -0500

    Initial commit

---
+++ main.py
@@ -0,0 +1,13 @@
+import sys
+import pygame
+
+
+def main():
+    # Setup screen size
+    size = width, height = 320, 240
+    screen = pygame.display.set_mode(size)\
+
+
+
+if __name__ == "__main__":
+    main()
