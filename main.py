e2dbda05aa1a447370adbda6b63ff682bc9a9410
John Allison <jeallis2@ncsu.edu>
2020-11-08 23:50:35 -0500

    Refactor and creatinb bugs lol

--- main.py
+++ main.py
@@ -1,13 +1,9 @@
 import sys
-from typing import List
-from object import physics_object
+
 import pygame
+import pygame_gui
 
-
-def spawn_object(objects, vel_x: float, vel_y: float, accel_x: float, accel_y: float):
-    obj = physics_object(vel_y, vel_x, accel_x, accel_y, "assets/ball.png")
-    objects.append(obj)
-    return objects
+from object import spawn_object
 
 
 def main():
@@ -20,15 +16,35 @@ def main():
     # Objects currently in the "world"
     world_objects = []
 
+    # GUI manager
+    manager = pygame_gui.UIManager((800, 600))
+    clock = pygame.time.Clock()
+
+    # TODO: Figure out why this isn't working lol
+    # Add a button the screen
+    hello_button = pygame_gui.elements.UIButton(relative_rect=pygame.Rect((350, 275), (100, 50)),
+                                                text='Say Hello',
+                                                manager=manager)
+
     # Test spawning an obj
     world_objects = spawn_object(world_objects, 5.0, 5.0, 0, 0)
 
     # Main loop
     while True:
-        # Check for exit condition
+        time_delta = clock.tick(60) / 1000.0
+
+        # Handle events
         for event in pygame.event.get():
+            # Check for exit condition
             if event.type == pygame.QUIT:
                 sys.exit()
+            # Handle button presses
+            if event.type == pygame.USEREVENT:
+                if event.user_type == pygame_gui.UI_BUTTON_PRESSED:
+                    if event.ui_element == hello_button:
+                        print('Hello World!')
+            # Handle GUI updates
+            manager.process_events(event)
 
         # Calculate movement for each object and apply it, then render
         for obj in world_objects:
@@ -43,6 +59,10 @@ def main():
             screen.fill(black)
             screen.blit(obj.surface, obj.rect)
             pygame.display.flip()
+
+        # Update and draw GUI
+        manager.update(time_delta)
+        manager.draw_ui(screen)
 
 
 if __name__ == "__main__":
--- object.py
+++ object.py
@@ -1,20 +1,25 @@
 from typing import List
 
-from pygame import Rect, Surface
 import pygame
 
 
-class physics_object:
+class PhysicsObject:
     """
     A physical object in the world. Has attributes for velocity in X and Y, acceleration in X and Y,
     and a related asset. The object does not actually get drawn on the screen, but is instead a
     centralized store of information for objects that will be handled by PyGame.
+
+    :param velocity_x The object's x velocity
+    :param velocity_y The object's y velocity
+    :param accel_x The object's x acceleration
+    :param accel_y The object's y acceleration
+    :param asset_path Local path to the object's associated visual asset
     """
     speed: List[float]
     accel_x: float
     accel_y: float
-    surface: Surface
-    rect: Rect
+    surface: pygame.Surface
+    rect: pygame.Rect
 
     def __init__(self, velocity_x: float, velocity_y: float, accel_x: float, accel_y: float, asset_path: str):
         self.speed = [velocity_x, velocity_y]
@@ -22,3 +27,13 @@ class physics_object:
         self.accel_y = accel_y
         self.surface = pygame.image.load(asset_path)
         self.rect = self.surface.get_rect()
+
+
+def spawn_object(objects, vel_x: float, vel_y: float, accel_x: float, accel_y: float) -> List[PhysicsObject]:
+    """
+    Takes a list of physics objects as a parameter and then returns a list appended with a new 
+    object with the given parameters. 
+    """
+    obj = PhysicsObject(vel_y, vel_x, accel_x, accel_y, "assets/ball.png")
+    objects.append(obj)
+    return objects
