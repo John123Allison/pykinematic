4932d8276245fd928db6075f0ee0cc24eee3b269
John Allison <jeallis2@ncsu.edu>
2020-11-09 20:20:41 -0500

    Get rid of all the comments that clogged up the code

--- main.py
+++ main.py
@@ -7,28 +7,27 @@ from object import spawn_object
 
 
 def main():
+    # Setup
     pygame.init()
-
-    # Setup screen size
     size = width, height = 1900, 1000
     screen = pygame.display.set_mode(size)
-    # Background color as a tuple
     black = 0, 0, 0
-
-    # Objects currently in the "world"
     world_objects = []
 
-    # TODO: GUI elements
+    # TODO: Setup GUI elements
     manager = pygame_gui.UIManager(size)
-    slider_rect = pygame.Rect(30, 20, 1000, 20)
-    x_acceleration_slider = pygame_gui.elements.UIHorizontalSlider(relative_rect=slider_rect,
-                                                start_value=0, value_range=(0,1), manager=manager)
 
-    # Track time delta
+    x_accel_slider_rect = pygame.Rect(30, 20, 1000, 20)
+    x_acceleration_slider = pygame_gui.elements.UIHorizontalSlider(relative_rect=x_accel_slider_rect,
+                                                                   start_value=0, value_range=(0, 1), manager=manager)
+    x_accel_label_rect = pygame.Rect(60, 60, 40, 40)
+    x_acceleration_label = pygame_gui.elements.UILabel(relative_rect=x_accel_label_rect,
+                                                       text="", manager=manager)
+
     clock = pygame.time.Clock()
 
     # Test spawning an obj
-    world_objects = spawn_object(world_objects, 0, 0, 0, .98)
+    world_objects = spawn_object(world_objects, 0, 0, .5, .98)
 
     # Main loop
     while True:
@@ -36,7 +35,6 @@ def main():
 
         # Handle events
         for event in pygame.event.get():
-            # Check for exit condition
             if event.type == pygame.QUIT:
                 sys.exit()
             # Handle x_acceleration_slider changes
@@ -44,21 +42,21 @@ def main():
                 if event.user_type == pygame_gui.UI_HORIZONTAL_SLIDER_MOVED:
                     if event.ui_element == x_acceleration_slider:
                         obj.accel_x = event.value
+                        x_acceleration_label.text = event.value
             # Handle GUI events
             manager.process_events(event)
 
         # Calculate movement for each object and apply it, then render
         for obj in world_objects:
-            # Apply acceleration to object's movement
             obj.speed[0] += obj.accel_x
             obj.speed[1] += obj.accel_y
-            # Apply movement and "bounce" off of walls
+
             obj.rect = obj.rect.move(obj.speed)
             if obj.rect.left < 0 or obj.rect.right > width:
                 obj.speed[0] = -obj.speed[0]
             if obj.rect.top < 0 or obj.rect.bottom > height:
                 obj.speed[1] = -obj.speed[1]
-            # Fill the screen and display
+
             screen.fill(black)
             screen.blit(obj.surface, obj.rect)
 
